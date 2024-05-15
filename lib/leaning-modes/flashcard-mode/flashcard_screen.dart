29847250b1d1a5539bb2a97e926c5d933e4d0dd3
thinhisme1123 <kieplulanh93@gmail.com>
2024-05-16 01:13:15 +0700

    Done read the word

--- lib/leaning-modes/flashcard-mode/flashcard_screen.dart
+++ lib/leaning-modes/flashcard-mode/flashcard_screen.dart
@@ -6,12 +6,11 @@ import '../../model/Word.dart';
 import 'flashcard.dart';
 import 'flashcard_view.dart';
 import 'package:flip_card/flip_card.dart';
-
+import 'package:flutter_tts/flutter_tts.dart';
 
 class FlashCardScreen extends StatefulWidget {
   final String topicID;
-  const FlashCardScreen({Key? key, required this.topicID})
-      : super(key: key);
+  const FlashCardScreen({Key? key, required this.topicID}) : super(key: key);
   @override
   State<StatefulWidget> createState() => _FlashCardScreenState();
 }
@@ -21,12 +20,28 @@ class _FlashCardScreenState extends State<FlashCardScreen> {
   late List<Flashcard> _flashcards;
   int _currentIndex = 0;
   bool _isLoading = true;
+  late FlutterTts _flutterTts;
 
   @override
   void initState() {
     super.initState();
     _pageViewController = PageController(initialPage: _currentIndex);
+    _flutterTts = FlutterTts();
     _loadWords();
+  }
+
+  Future _speakText(String text) async {
+    final containsVietnamese = RegExp(
+        r'[àáạảãâầẩẫậăằắẳẵặèéẻẽẹêềếểễệìíỉĩịòóọỏõôồốổỗộơờớởỡợùúụủũưừứửữựỳýỷỹỵ]');
+    final isVietnamese = containsVietnamese.hasMatch(text);
+
+    if (isVietnamese) {
+      await _flutterTts.setLanguage('vi-VN');
+    } else {
+      await _flutterTts.setLanguage('en-US');
+    }
+    await _flutterTts.setPitch(1.0);
+    await _flutterTts.speak(text);
   }
 
   Future<void> _loadWords() async {
@@ -55,91 +70,94 @@ class _FlashCardScreenState extends State<FlashCardScreen> {
         appBar: AppBar(
           backgroundColor: const Color(0xFF51C5F5),
           actions: [
-            IconButton(onPressed: () {
-
-
-            }, icon: Icon(Icons.speaker))
+            IconButton(onPressed: () {}, icon: Icon(Icons.speaker))
           ], // Background color of the AppBar
         ),
         backgroundColor: const Color(0xFF51C5F5),
         body: Center(
-          child:_isLoading
-            ? CircularProgressIndicator()
-            : Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 300,
-                height: 200,
-                child: PageView.builder(
-                  controller: _pageViewController,
-                  itemCount: _flashcards.length,
-                  onPageChanged: (index) {
-                    setState(() {
-                      _currentIndex = index;
-                    });
-                  },
-                  itemBuilder: (context, index) {
-                    return FlipCard(
-                      key: ValueKey(_flashcards[index]),
-                      front: FlashcardView(
-                        text: _flashcards[index].question,
+          child: _isLoading
+              ? CircularProgressIndicator()
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 300,
+                      height: 200,
+                      child: PageView.builder(
+                        controller: _pageViewController,
+                        itemCount: _flashcards.length,
+                        onPageChanged: (index) {
+                          setState(() {
+                            _currentIndex = index;
+                          });
+                        },
+                        itemBuilder: (context, index) {
+                          return FlipCard(
+                            key: ValueKey(_flashcards[index]),
+                            front: FlashcardView(
+                              text: _flashcards[index].question,
+                            ),
+                            back: FlashcardView(
+                              text: _flashcards[index].answer,
+                              onFlip: () =>
+                                  _speakText(_flashcards[index].answer),
+                            ),
+                          );
+                        },
                       ),
-                      back: FlashcardView(
-                        text: _flashcards[index].answer,
-                      ),
-                    );
-                  },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        OutlinedButton.icon(
+                          onPressed: showPreviousCard,
+                          icon: Icon(Icons.chevron_left),
+                          label: Text('Prev'),
+                        ),
+                        Text(
+                          '${_currentIndex + 1}/${_flashcards.length}',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        OutlinedButton.icon(
+                          onPressed: showNextCard,
+                          icon: Icon(Icons.chevron_right),
+                          label: Text('Next'),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 20,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  OutlinedButton.icon(
-                    onPressed: showPreviousCard,
-                    icon: Icon(Icons.chevron_left),
-                    label: Text('Prev'),
-                  ),
-                  Text(
-                    '${_currentIndex + 1}/${_flashcards.length}',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  OutlinedButton.icon(
-                    onPressed: showNextCard,
-                    icon: Icon(Icons.chevron_right),
-                    label: Text('Next'),
-                  ),
-                ],
-              ),
-            ],
-          ),
         ),
       ),
     );
   }
 
   void showNextCard() {
-  setState(() {
-    if (_currentIndex + 1 < _flashcards.length) {
-      _currentIndex++;
-      _pageViewController.nextPage(duration: Duration(milliseconds: 500), curve: Curves.linear);
-    } else {
-      _currentIndex = 0;
-      _pageViewController.animateToPage(
-        0,
-        duration: Duration(milliseconds: 500),
-        curve: Curves.linear,
-      );
-    }
-  });
-}
+    setState(() {
+      if (_currentIndex + 1 < _flashcards.length) {
+        _currentIndex++;
+        _pageViewController.nextPage(
+            duration: Duration(milliseconds: 500), curve: Curves.linear);
+      } else {
+        _currentIndex = 0;
+        _pageViewController.animateToPage(
+          0,
+          duration: Duration(milliseconds: 500),
+          curve: Curves.linear,
+        );
+      }
+    });
+  }
 
   void showPreviousCard() {
     setState(() {
       if (_currentIndex - 1 >= 0) {
         _currentIndex--;
-        _pageViewController.previousPage(duration: Duration(milliseconds: 500), curve: Curves.linear);
+        _pageViewController.previousPage(
+            duration: Duration(milliseconds: 500), curve: Curves.linear);
       } else {
         // If we are on the first flashcard, go to the last one
         _currentIndex = _flashcards.length - 1;
@@ -153,4 +171,3 @@ class _FlashCardScreenState extends State<FlashCardScreen> {
     });
   }
 }
- 
